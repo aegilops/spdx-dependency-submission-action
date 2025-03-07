84ec6612af7fa4d5cca4921feaa61ba66b2c1e06
aegilops <41705651+aegilops@users.noreply.github.com>
2025-03-07 16:54:11 +0000

    npm install

--- dist/index.js
+++ dist/index.js
@@ -35603,13 +35603,15 @@ async function run() {
       ref = `refs/heads/${ref}`;
     }
 
+    core.notice(`Submitting snapshot for ref ${ref}`);
+
     // get the SHA of the ref, using git
     const gitFetch = execFileSync('git', ['fetch', '--depth=1', 'origin', ref], {
         stdio: 'pipe',
         encoding: 'utf8',
       });
 
-    core.debug(`git fetch output: ${gitFetch}`);
+    core.notice(`git fetch output: ${gitFetch}`);
 
     sha = execFileSync('git', ['show', '-s', '--format="%H"', ref], {
         stdio: 'pipe',
